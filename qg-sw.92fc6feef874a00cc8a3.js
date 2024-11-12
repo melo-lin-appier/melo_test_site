db31477e275dadb9c374f5e6b8b7eae855c07673
melo.lin <melo.lin@appier.com>
2024-11-12 10:50:18 +0800

    add mellow testing account

--- qg-sw.92fc6feef874a00cc8a3.js
+++
@@ -1,2 +0,0 @@
-var appId = "92fc6feef874a00cc8a3";
-importScripts("https://aiqua-stg-qgraph.s3-ap-southeast-1.amazonaws.com/v3/r/qg-sw.js");
---
+++ qg-sw.ca8b508a44c4cd8816f5.js
@@ -0,0 +1,2 @@
+var appId = "ca8b508a44c4cd8816f5";
+importScripts("https://aiqua-stg-qgraph.s3-ap-southeast-1.amazonaws.com/v3/r/qg-sw.js");
