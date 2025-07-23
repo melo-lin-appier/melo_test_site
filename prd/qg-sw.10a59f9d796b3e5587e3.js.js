18eec1592da8cd8bb19f670beaabd1070221a50e
melo.lin <melo.lin@appier.com>
2025-07-23 14:01:47 +0800

    feature: add production site

---
+++ prd/qg-sw.10a59f9d796b3e5587e3.js.js
@@ -0,0 +1,2 @@
+var appId = "10a59f9d796b3e5587e3";
+importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
---
+++ stg/qg-sw.92fc6feef874a00cc8a3.js
@@ -0,0 +1,2 @@
+var appId = "92fc6feef874a00cc8a3";
+importScripts("https://aiqua-stg-qgraph.s3-ap-southeast-1.amazonaws.com/v3/r/qg-sw.js");
